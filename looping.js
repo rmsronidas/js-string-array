96416bdf5e6ccddfeae944a2b1de2648f60e9575
rmsronidas <rmsronidas@gmail.com>
2026-01-29 20:01:13 +0600

    looing added

---
+++ looping.js
@@ -0,0 +1,16 @@
+const friends =['a','b','c','d']
+
+for(const friend of friends){
+    // console.log(friend);
+}
+
+// for (let i=0; i<friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i]);
+// }
+
+const numbers =[2,3,4,54,5,4,2,7]
+
+for (let i =0; i<numbers.length; i++){
+    console.log(numbers[i]);
+}
